c79cbeb0ea5e828b6f7eb497af33471cbe4744b1
Colin Zarzycki <zarzycki@ucar.edu>
2018-08-06 13:15:08 -0700

    generalization of extract_individual_storms incl. low-mem

---
+++ tracking/hyperion-snow-tracker.sh
@@ -0,0 +1,224 @@
+#!/bin/bash
+
+# Script to A) track storms and then B) extract snowfall/calculate RSI values and other statistics
+# from LENS member (NUMID). 
+#
+# Colin Zarzycki (zarzycki@ucar.edu)
+#
+# Usage is ./lens-snow-tracker $NUMID
+# Driven by "batch-loginnodes.sh" for parallel processing of LENS data
+
+date
+
+NUMID=$1
+
+DO_TEMPEST=false           # Do we do Lagrangian/Tempest trajectories?
+DO_CONNTRACK=false        # Do we do "connected" trajectories?
+PROCESS_TEMPEST=false      # Do we extract/calc RSI for Lagrangian/Tempest trajectories?
+PROCESS_CONNTRACK=true   # Do we extract/calc RSI for "connected" trajectories?
+SEASONFRAC_TEMPEST=false
+SEASONFRAC_CONNTRACK=false
+
+SWE="12"                   # snow water equivalent (12 = default, 9-13 is probably acceptable range)
+THRESH="5e-9"              # Connected trajectory threshold (area average precipitation)
+
+MODEL="LENS"
+RUNDATES="2071010100Z-2080123118Z"
+RUNCONFIG="BRCP85C5CNBDRD"
+CONFIG=${THRESH}"_"${SWE}
+
+DESCSTR=${MODEL}"."${RUNDATES:0:4}"."${NUMID}"."${CONFIG}
+
+RSIDIR=/glade/u/home/zarzycki/snow-tracking/calc_RSI/
+RSISNOWFILE="RSI.SNOW."${DESCSTR}".csv"
+RSIPRECFILE="RSI.PREC."${DESCSTR}".csv"
+
+NCLPATH=/glade/u/home/zarzycki/snow-tracking/tracking/
+RSIOUTDIR=/glade/scratch/zarzycki/LES-snow/stats/
+EXTRACTOUTDIR=/glade/scratch/zarzycki/LES-snow/storm-files/
+
+PRECTFILE="/glade/p_old/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/hourly6/PRECT/b.e11."${RUNCONFIG}".f09_g16."${NUMID}".cam.h2.PRECT."${RUNDATES}".nc"
+PRECVARNAME="PRECT"
+PSLFILE="/glade/p_old/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/hourly6/PSL/b.e11."${RUNCONFIG}".f09_g16."${NUMID}".cam.h2.PSL."${RUNDATES}".nc"
+PSLVARNAME="PSL"
+SNOWFILE="/glade/u/home/zarzycki/scratch/LENS-snow/b.e11."${RUNCONFIG}".f09_g16."${NUMID}".cam.h2.PRECT_SNOW."${RUNDATES}".nc"
+SNOWVARNAME="PRECT_SNOW"
+
+TRAJFILE="./traj/conntraj."${DESCSTR}
+EXTRACTOUTFILE="ind-storms."${DESCSTR}".nc"
+
+DATESTRING=`date +"%s%N"`
+
+TEMPESTFILE="./traj/tempest."${MODEL}"."${RUNDATES:0:4}"."${NUMID}
+TMPTRAJNAME="./traj/trajectories.txt."${MODEL}"."${RUNDATES:0:4}"."${NUMID}
+
+starttime=$(date -u +"%s")
+
+###################################################################################################
+
+if [ ${DO_TEMPEST} == 'true' ]; then
+
+  starttimetrack=$(date -u +"%s")
+
+  BUILDDIR=/glade/work/zarzycki/tempestextremes_noMPI/bin/
+
+  rm cyc.${DATESTRING}
+  rm ${TMPTRAJNAME}
+  touch cyc.${DATESTRING}
+
+  FILELISTNAME=filelist.txt.${DATESTRING}
+  ls "/glade/p_old/cesmLE/CESM-CAM5-BGC-LE/atm/proc/tseries/hourly6/PSL/b.e11."${RUNCONFIG}".f09_g16."${NUMID}".cam.h2.PSL."${RUNDATES}".nc" -1 > ${FILELISTNAME}
+
+  ${BUILDDIR}/DetectCyclonesUnstructured --in_data_list "${FILELISTNAME}" --out cyc_tempest.${DATESTRING} --closedcontourcmd "PSL,200.,6.,0" --outputcmd "PSL,min,0" --maxlat 80. --minlat 10. </dev/null
+  cat cyc_tempest.${DATESTRING}* >> cyc.${DATESTRING}
+  rm cyc_tempest.${DATESTRING}*
+
+  rm ${FILELISTNAME}
+  rm log*
+
+  ${BUILDDIR}/StitchNodes --format "i,j,lon,lat,slp" --in cyc.${DATESTRING} --range 8.0 --minlength 5 --maxgap 0 --out ${TMPTRAJNAME} --threshold "lat,>,25,3;lon,>,260,3;lat,<,65,3;lon,<,310,3"
+
+  #BUILDDIR=/glade/p_old/work/zarzycki/tempestextremes/bin/
+  #${BUILDDIR}/StitchNodes --format "i,j,lon,lat,slp" --in cyc.1505498447694611008 --range 8.0 --minlength 5 --maxgap 0 --out trajectories.txt.LENS.5e-9_12.001 --threshold "lat,>,25,3;lon,>,260,3;lat,<,65,3;lon,<,310,3"
+
+  endtimetrack=$(date -u +"%s")
+
+  # Apparently need this to make sure stuff is written to disk?
+  sleep 5
+
+  starttimefilter=$(date -u +"%s")
+
+  ncl filter_ne_storms.ncl 'infile="'${TMPTRAJNAME}'"' \
+   'outfile="'${TEMPESTFILE}'"'
+
+  rm cyc.${DATESTRING}
+  rm ${TMPTRAJNAME}
+
+  endtimefilter=$(date -u +"%s")
+
+fi # end do tempest
+
+
+if [ ${DO_CONNTRACK} == 'true' ]; then
+  starttimefilter=$(date -u +"%s")
+
+  ncl ${NCLPATH}/connected-tracking.ncl 'snowFileFull="'${PRECTFILE}'"' \
+     'outfile="'${TRAJFILE}'"' \
+     'thresh_prec_str="'${THRESH}'"'
+
+  endtimetrack=$(date -u +"%s")
+fi
+
+###################################################################################################
+
+if [ ${SEASONFRAC_TEMPEST} == 'true' ]; then
+  OUTFRACFILE="frac.tempest."${DESCSTR}".nc"
+  (set -x; ncl ${NCLPATH}/calc-seasonal-fraction.ncl 'snowFileFull="'${SNOWFILE}'"' \
+    'SNOWVARNAME="'${SNOWVARNAME}'"' \
+    'prectFileFull="'${PRECTFILE}'"' \
+    'traj_filename="'${TEMPESTFILE}'"' \
+    'outDir="'${EXTRACTOUTDIR}'"' \
+    'outFileName="'${OUTFRACFILE}'"')
+fi
+
+if [ ${SEASONFRAC_CONNTRACK} == 'true' ]; then
+  OUTFRACFILE="frac.conntraj."${DESCSTR}".nc"
+  (set -x; ncl ${NCLPATH}/calc-seasonal-fraction.ncl 'snowFileFull="'${SNOWFILE}'"' \
+    'SNOWVARNAME="'${SNOWVARNAME}'"' \
+    'prectFileFull="'${PRECTFILE}'"' \
+    'traj_filename="'${TRAJFILE}'"' \
+    'outDir="'${EXTRACTOUTDIR}'"' \
+    'outFileName="'${OUTFRACFILE}'"')
+fi
+
+###################################################################################################
+
+if [ ${PROCESS_CONNTRACK} == 'true' ]; then
+
+  starttimeextract=$(date -u +"%s")
+
+  (set -x; ncl ${NCLPATH}/extract_individual_storms.ncl 'snowFileFull="'${SNOWFILE}'"' \
+    'SNOWVARNAME="'${SNOWVARNAME}'"' \
+    'prectFileFull="'${PRECTFILE}'"' \
+    'PRECTVARNAME="'${PRECVARNAME}'"' \
+    'pslFileFull="'${PSLFILE}'"' \
+    'PSLVARNAME="'${PSLVARNAME}'"' \
+    'traj_filename="'${TRAJFILE}'"' \
+    'outDir="'${EXTRACTOUTDIR}'"' \
+    'outFileName="'${EXTRACTOUTFILE}'"')
+
+   endtimeextract=$(date -u +"%s")
+   starttimeRSI=$(date -u +"%s")
+
+  RSISNOWFILES=(${RSISNOWFILE}".SNOW.conntraj.csv" ${RSISNOWFILE}".PRECT.conntraj.csv")
+  SWES=(${SWE} ${SWE})
+  SNOWVARNAMES=("CUM_SNOWFALL" "CUM_PRECT")
+  # LOOPVAR does both snow and total precip (pretending total precip is snow)
+  for LOOPVAR in `seq 0 1`
+  do
+    rm ${RSIOUTDIR}/${RSISNOWFILES[${LOOPVAR}]} 
+    LOOPST=0
+    LOOPEN=$((`grep -c start ${TRAJFILE}` - 1))
+    for IX in `seq ${LOOPST} ${LOOPEN}`
+    do
+      (set -x; ncl ${RSIDIR}/calculate_RSI.ncl stormID=${IX} SWE=${SWES[${LOOPVAR}]} \
+       'imgDir="'${RSIOUTDIR}'/images-RSI-tempest/'${MODEL}'.'${RUNDATES:0:4}'.'${NUMID}'"' \
+       'RSIoutFile="'${RSIOUTDIR}/${RSISNOWFILES[${LOOPVAR}]}'"' \
+       'stormFilePath="'${EXTRACTOUTDIR}/${EXTRACTOUTFILE}'"' \
+       'configStr="'${DESCSTR}'"' \
+       'SNOWVARNAME="'${SNOWVARNAMES[${LOOPVAR}]}'"')
+    done
+  done
+  endtimeRSI=$(date -u +"%s")
+fi
+
+###################################################################################################
+
+if [ ${PROCESS_TEMPEST} == 'true' ]; then
+  starttimeextract=$(date -u +"%s")
+  TRAJFILE=${TEMPESTFILE}
+  EXTRACTOUTFILE=${EXTRACTOUTFILE}".tempest.nc"
+  ncl ${NCLPATH}/extract_individual_storms.ncl 'snowFileFull="'${SNOWFILE}'"' \
+    'SNOWVARNAME="'${SNOWVARNAME}'"' \
+    'prectFileFull="'${PRECTFILE}'"' \
+    'PRECTVARNAME="'${PRECVARNAME}'"' \
+    'pslFileFull="'${PSLFILE}'"' \
+    'PSLVARNAME="'${PSLVARNAME}'"' \
+    'traj_filename="'${TRAJFILE}'"' \
+    'outDir="'${EXTRACTOUTDIR}'"' \
+    'outFileName="'${EXTRACTOUTFILE}'"'
+  endtimeextract=$(date -u +"%s")
+
+  starttimeRSI=$(date -u +"%s")
+  RSISNOWFILES=(${RSISNOWFILE}".SNOW.tempest.csv" ${RSISNOWFILE}".PRECT.tempest.csv")
+  SWES=(${SWE} ${SWE})
+  SNOWVARNAMES=("CUM_SNOWFALL" "CUM_PRECT")
+  # LOOPVAR does both snow and total precip (pretending total precip is snow)
+  for LOOPVAR in `seq 0 1`
+  do
+    rm ${RSIOUTDIR}/${RSISNOWFILES[${LOOPVAR}]} 
+    LOOPST=0
+    LOOPEN=$((`grep -c start ${TRAJFILE}` - 1))
+    for IX in `seq ${LOOPST} ${LOOPEN}`
+    do
+      (set -x; ncl ${RSIDIR}/calculate_RSI.ncl stormID=${IX} SWE=${SWES[${LOOPVAR}]} \
+       'imgDir="'${RSIOUTDIR}'/images-RSI-tempest/'${MODEL}'.'${RUNDATES:0:4}'.'${NUMID}'"' \
+       'RSIoutFile="'${RSIOUTDIR}/${RSISNOWFILES[${LOOPVAR}]}'"' \
+       'stormFilePath="'${EXTRACTOUTDIR}/${EXTRACTOUTFILE}'"' \
+       'configStr="'${DESCSTR}'"' \
+       'SNOWVARNAME="'${SNOWVARNAMES[${LOOPVAR}]}'"')
+    done
+  done
+  endtimeRSI=$(date -u +"%s")
+fi
+
+# Calculate timings for each segment by checking if starttime was defined and then calculating if defined
+if [ -z ${starttimetrack+x} ]; then tottimetrack=0; else tottimetrack=$(($endtimetrack-$starttimetrack)); fi
+if [ -z ${starttimefilter+x} ]; then tottimefilter=0; else tottimefilter=$(($endtimefilter-$starttimefilter)); fi
+if [ -z ${starttimeextract+x} ]; then tottimeextract=0; else tottimeextract=$(($endtimeextract-$starttimeextract)); fi
+if [ -z ${starttimeRSI+x} ]; then tottimeRSI=0; else tottimeRSI=$(($endtimeRSI-$starttimeRSI)); fi
+
+endtime=$(date -u +"%s")
+tottime=$(($endtime-$starttime))
+
+printf "${RUNDATES:0:4} ${NUMID} ${tottimetrack} ${tottimefilter} ${tottimeextract} ${tottimeRSI} ${tottime}\n" >> timing.txt
---
+++ tracking/par-tracking.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -l
+
+################################################################
+#SBATCH -N 1                #Number of nodes
+#SBATCH -t 01:00:00         #Time limit
+#SBATCH -q premium          #Use the regular QOS
+#SBATCH -L SCRATCH          #Job requires $SCRATCH file system
+#SBATCH -C knl,quad,cache   #Use KNL nodes in quad cache format (default, recommended)
+################################################################
+
+# salloc -N 1 -C knl -q interactive -t 01:00:00
+
+############ USER OPTIONS #####################
+
+## Path to TempestExtremes binaries on YS
+TEMPESTEXTREMESDIR=~/software/tempestextremes/
+
+CONFIG=dtime900.003
+MACH=CORI
+GRID=WAT
+UQSTR=${MACH}.VR28.NATL.${GRID}.CAM5.4CLM5.0.${CONFIG}
+TOPOFILE=/global/homes/c/czarzyck/scratch/unigridFiles/ne0np4natlantic${GRID,,}.ne30x4/topo/topo_ne0np4natlantic${GRID,,}.ne30x4_interpic.nc
+PATHTOFILES=/global/homes/c/czarzyck/scratch/hyperion/${MACH}.VR28.NATL.${GRID}.CAM5.4CLM5.0.${CONFIG}/atm/hist/
+CONNECTFLAG="--in_connect /global/homes/c/czarzyck/tempest-scripts/hyperion/ne0np4natlantic${GRID,,}.ne30x4.connect.dat" 
+
+############ TRACKER MECHANICS #####################
+
+DATESTRING=`date +"%s%N"`
+FILELISTNAME=filelist.txt.${DATESTRING}
+TRAJFILENAME=trajectories.txt.${UQSTR}
+touch $FILELISTNAME
+
+# Generate parallel file
+FILES=${PATHTOFILES}/*.h3.1986*.nc
+for f in $FILES
+do
+  echo "${f};${TOPOFILE}" >> $FILELISTNAME
+done
+
+starttime=$(date -u +"%s")
+
+STR_DETECT="--verbosity 0 --timestride 2 ${CONNECTFLAG} --out cyclones_tempest.${DATESTRING} --mergedist 8.0 --closedcontourcmd PSL,200.,6.,0 --outputcmd PSL,min,0"
+touch cyclones.${DATESTRING}
+srun -n 32 ${TEMPESTEXTREMESDIR}/bin/DetectCyclonesUnstructured --in_data_list "${FILELISTNAME}" ${STR_DETECT} </dev/null
+cat cyclones_tempest.${DATESTRING}* >> cyclones.${DATESTRING}
+rm cyclones_tempest.${DATESTRING}*
+
+# Stitch candidate cyclones together
+${TEMPESTEXTREMESDIR}/bin/StitchNodes --format "ncol,lon,lat,slp" --in cyclones.${DATESTRING} --range 8.0 --minlength 5 --maxgap 0 --out ${TRAJFILENAME} --threshold "lat,>,25,3;lon,>,260,3;lat,<,65,3;lon,<,310,3"
+
+endtime=$(date -u +"%s")
+tottime=$(($endtime-$starttime))
+
+printf "${tottime}\n" >> timing.txt
+
+rm cyclones.${DATESTRING}
+rm ${FILELISTNAME}
+rm log*
