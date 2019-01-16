8b23830bc89fb7c436e93254c7ceafb19cf2fa9b
Colin Zarzycki <zarzycki@ucar.edu>
2019-01-16 08:42:00 -0700

    change namelist fcs in NCL so don't need to end with comma

--- example/driver.sh
+++ example/driver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ESTAPATH=/glade/home/zarzycki/snow-tracking/
+ESTAPATH=/glade/u/home/zarzycki/snow-tracking/
 NAMELIST=nl.jra.sample
 
 ${ESTAPATH}/tracking/esta-etc-tracking.sh ${NAMELIST} 
 ${ESTAPATH}/tracking/esta-extract-storms.sh ${NAMELIST}
-${ESTAPATH}/tracking/esta-calc-RSI.sh ${NAMELIST}"
+${ESTAPATH}/tracking/esta-calc-RSI.sh ${NAMELIST}
